ca29ef8881606c8410cf001b4f54c875bb06ade2
Martin Kruusement <martinkruusement@gmail.com>
2022-05-18 17:55:19 +0300

    Created removeIslands

---
+++ index.js
@@ -0,0 +1,13 @@
+const cases = require('./load-tests.js')
+
+function solution (A) {
+  let input = A
+  let output = null
+
+  return output
+}
+
+// eslint-disable-next-line no-unused-expressions
+cases[0].in
+solution(cases[0].in)
+module.exports = solution
